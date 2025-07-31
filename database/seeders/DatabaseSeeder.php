7d66ec2b03099212f7353def5f5699343e9f452b
Jose Hernandez <hernandezmartinezjose2017@gmail.com>
2025-07-30 22:32:21 -0600

    integracion seed usuario

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,8 +14,9 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-
+        $this->call([
+            UserSeeder::class,
+        ]);
         $this->call(ProductoSeeder::class);
-
     }
 }
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        // Admin
+        User::updateOrCreate(
+            ['email' => 'admin@cybersandoval.com'],
+            [
+                'name' => 'Administrador',
+                'password' => Hash::make('admin123'),
+                'rol' => 'admin'
+            ]
+        );
+
+        // Cajero
+        User::updateOrCreate(
+            ['email' => 'cajero@cybersandoval.com'],
+            [
+                'name' => 'Cajero',
+                'password' => Hash::make('cajero123'),
+                'rol' => 'cajero'
+            ]
+        );
+    }
+}
